bb8bae55f8f419e3859043405a5d68fb05ad5e96
modhu123454321-cmd <modhu123454321@gmail.com>
2026-09-23 01:08:16 +0530

    Add readTextFile function declaration

--- src/file_utils.h
+++ src/file_utils.h
@@ -5,3 +5,4 @@ bool fileExists(const std::string& path);
 bool ensureDirectoryExists(const std::string& path);
 bool writeTextFile(const std::string& path, const std::string& content);
 bool ensureDirectoriesExist(const std::string& path);
+bool readTextFile(const std::string& path, std::string& outContent);
