010738f3e2b80bde4d4f65d446697d52ee6e1fe6
modhu123454321-cmd <modhu123454321@gmail.com>
2026-09-24 00:10:28 +0530

    Change readTextFile to use output parameter

--- src/file_utils.h
+++ src/file_utils.h
@@ -3,4 +3,4 @@
 #include <string>
 
 std::string trim(const std::string& input);
-std::string readTextFile(const std::string& path, bool binary = false);
+bool readTextFile(const std::string& path, std::string& outText, bool binary = false);
