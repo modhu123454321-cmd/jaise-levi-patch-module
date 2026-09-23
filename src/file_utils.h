76a6e9a92d9b65c89c466387cd2a61e3f3425af8
modhu123454321-cmd <modhu123454321@gmail.com>
2026-09-24 00:06:30 +0530

    Modify readTextFile to include binary option

--- src/file_utils.h
+++ src/file_utils.h
@@ -3,4 +3,4 @@
 #include <string>
 
 std::string trim(const std::string& input);
-std::string readTextFile(const std::string& path);
+std::string readTextFile(const std::string& path, bool binary = false);
